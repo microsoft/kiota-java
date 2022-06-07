dc79618cfa600b2a25121e831908eab6c3c65c0b
ramsessanchez <ramses.sanchez99@gmail.com>
2022-06-07 15:22:42 -0700

    pass in workflow paths

--- components/abstractions/scripts/decodeAndWrite.ps1
+++ components/abstractions/scripts/decodeAndWrite.ps1
@@ -18,9 +18,6 @@ Param(
     [string]$outputPath
 )
 
-Write-Output "encodedVal: $encodedValue"
-Write-Output "outPath: $outputPath"
-
 if($outputPath -eq "" -or $null -eq $outputPath) {
     Write-Output "Value of Variable: outputPath is Null or Empty. Exiting."
     Exit 
