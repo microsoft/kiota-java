dc7e53ba11996ee1a9492146bd9aa3cef2f4a0bc
ramsessanchez <ramses.sanchez99@gmail.com>
2022-12-14 11:23:14 -0800

    native response handler

--- components/abstractions/src/main/java/com/microsoft/kiota/NativeResponseHandler.java
+++ components/abstractions/src/main/java/com/microsoft/kiota/NativeResponseHandler.java
@@ -9,18 +9,29 @@ import javax.annotation.Nullable;
 
 import com.microsoft.kiota.serialization.Parsable;
 import com.microsoft.kiota.serialization.ParsableFactory;
-
 public class NativeResponseHandler implements ResponseHandler {
 
-    public Object value;
-
-    public HashMap<String, ParsableFactory<? extends Parsable>> errorMappings;
+    private Object value;
+    private HashMap<String, ParsableFactory<? extends Parsable>> errorMappings;
 
     @Override
     @Nonnull
     public <NativeResponseType, ModelType> CompletableFuture<ModelType> handleResponseAsync(@Nonnull NativeResponseType response, @Nullable HashMap<String, ParsableFactory<? extends Parsable>> errorMappings) {
-        value = response;
-        this.errorMappings = errorMappings;
+        this.value = response;
+        if(errorMappings != null)
+            this.errorMappings = new HashMap<>(errorMappings);
         return CompletableFuture.completedFuture(null);
+    }
+    
+    public void setErrorMappings(HashMap<String, ParsableFactory<? extends Parsable>> errorMappings) {
+        this.errorMappings = new HashMap<>(errorMappings);
+    }
+
+    public HashMap<String, ParsableFactory<? extends Parsable>> getErrorMappings() {
+        return new HashMap<>(this.errorMappings);
+    }
+    
+    public Object getValue() {
+        return this.value;
     }
 }
