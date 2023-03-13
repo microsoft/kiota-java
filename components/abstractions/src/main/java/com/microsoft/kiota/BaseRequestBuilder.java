8e5a8ec3b079cc2d797a7d1665ac1ba6c4d8c563
Vincent Biret <vibiret@microsoft.com>
2023-03-13 13:41:16 -0400

    - adds a base request builder

---
+++ components/abstractions/src/main/java/com/microsoft/kiota/BaseRequestBuilder.java
@@ -0,0 +1,38 @@
+package com.microsoft.kiota;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+import javax.annotation.Nonnull;
+
+/** Base class for request builders */
+public abstract class BaseRequestBuilder {
+    /** Path parameters for the request */
+    @Nonnull
+    protected HashMap<String, Object> pathParameters;
+    /** The request adapter to use to execute the requests. */
+    @Nonnull
+    protected RequestAdapter requestAdapter;
+    /** Url template to use to build the URL for the current request builder */
+    @Nonnull
+    protected String urlTemplate;
+    /**
+     * Instantiates a new BaseRequestBuilder and sets the default values.
+     * @param requestAdapter The request adapter to use to execute the requests.
+     * @param urlTemplate Url template to use to build the URL for the current request builder
+     */
+    protected BaseRequestBuilder(@Nonnull final RequestAdapter requestAdapter, @Nonnull final String urlTemplate) {
+        this(requestAdapter, urlTemplate, new HashMap<String, Object>());
+    }
+    /**
+     * Instantiates a new BaseRequestBuilder and sets the default values.
+     * @param requestAdapter The request adapter to use to execute the requests.
+     * @param urlTemplate Url template to use to build the URL for the current request builder
+     * @param pathParameters Path parameters for the request
+     */
+    protected BaseRequestBuilder(@Nonnull final RequestAdapter requestAdapter, @Nonnull final String urlTemplate, @Nonnull final HashMap<String, Object> pathParameters) {
+        this.requestAdapter = Objects.requireNonNull(requestAdapter);
+        this.urlTemplate = Objects.requireNonNull(urlTemplate);
+        this.pathParameters = new HashMap<String, Object>(Objects.requireNonNull(pathParameters));
+    }
+}
