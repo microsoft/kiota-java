a3bc5488d44e5330d88c6385705035cf6c062d15
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2023-03-30 13:07:06 -0700

    comments

--- components/abstractions/src/main/java/com/microsoft/kiota/NativeResponseHandler.java
+++ components/abstractions/src/main/java/com/microsoft/kiota/NativeResponseHandler.java
@@ -10,10 +10,8 @@ import javax.annotation.Nullable;
 import com.microsoft.kiota.serialization.Parsable;
 import com.microsoft.kiota.serialization.ParsableFactory;
 public class NativeResponseHandler implements ResponseHandler {
-
     private Object value;
     private HashMap<String, ParsableFactory<? extends Parsable>> errorMappings;
-
     @Override
     @Nonnull
     public <NativeResponseType, ModelType> CompletableFuture<ModelType> handleResponseAsync(@Nonnull NativeResponseType response, @Nullable HashMap<String, ParsableFactory<? extends Parsable>> errorMappings) {
@@ -22,15 +20,24 @@ public class NativeResponseHandler implements ResponseHandler {
             this.errorMappings = new HashMap<>(errorMappings);
         return CompletableFuture.completedFuture(null);
     }
-    
+    /**
+     * Set the error mappings for the response to use when deserializing failed responses bodies.
+     * @param errorMappings The designated error mappings.
+     */
     public void setErrorMappings(HashMap<String, ParsableFactory<? extends Parsable>> errorMappings) {
         this.errorMappings = new HashMap<>(errorMappings);
     }
-
+    /**
+     * Get the error mappings for the response when deserializing failed response bodies.
+     * @return The error mappings for failed response bodies.
+     */
     public HashMap<String, ParsableFactory<? extends Parsable>> getErrorMappings() {
         return new HashMap<>(this.errorMappings);
     }
-    
+    /**
+     * Get the value for the response.
+     * @return the value of the response.
+     */
     public Object getValue() {
         return this.value;
     }
