f2b907b0751d2fb9cab9008966056d0c1694b516
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2023-03-31 12:05:40 -0700

    code smells

--- components/abstractions/src/main/java/com/microsoft/kiota/RequestInformation.java
+++ components/abstractions/src/main/java/com/microsoft/kiota/RequestInformation.java
@@ -201,8 +201,8 @@ public class RequestInformation {
         this.content = value;
         headers.add(CONTENT_TYPE_HEADER, BINARY_CONTENT_TYPE);
     }
-    private final String SERIALIZE_ERROR = "could not serialize payload";
-    private final String SPAN_NAME = "setContentFromParsable";
+    private static final String SERIALIZE_ERROR = "could not serialize payload";
+    private static final String SPAN_NAME = "setContentFromParsable";
     private static final String OBSERVABILITY_TRACER_NAME = "com.microsoft.kiota";
     /**
      * Sets the request body from a model with the specified content type.
