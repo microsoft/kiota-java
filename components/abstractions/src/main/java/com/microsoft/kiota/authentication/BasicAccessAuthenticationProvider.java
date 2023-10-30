3eecc997b8d7a9a23feeb3d2bd034c3eba4b76a7
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2023-10-30 18:09:51 +0000

    spotbugs

--- components/abstractions/src/main/java/com/microsoft/kiota/authentication/BasicAccessAuthenticationProvider.java
+++ components/abstractions/src/main/java/com/microsoft/kiota/authentication/BasicAccessAuthenticationProvider.java
@@ -9,22 +9,28 @@ import java.util.Base64;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
-// https://en.wikipedia.org/wiki/Basic_access_authentication
+/** Provides an implementation of the Basic Access Authentication scheme: https://en.wikipedia.org/wiki/Basic_access_authentication . */
 public class BasicAccessAuthenticationProvider implements AuthenticationProvider {
+    private final static String AUTHORIZATION_HEADER_KEY = "Authorization";
+    private static final String BASIC = "Basic ";
 
     private final String username;
     private final String password;
     private final String encoded;
 
-    public BasicAccessAuthenticationProvider(String username, String password) {
+    /**
+     * Instantiates a new BasicAccessAuthenticationProvider.
+     * @param username the username to be used.
+     * @param password the password to be used.
+     */
+    public BasicAccessAuthenticationProvider(@Nonnull final String username, @Nonnull final String password) {
         this.username = username;
         this.password = password;
         encoded = Base64.getEncoder().encodeToString((username + ":" + password).getBytes(StandardCharsets.UTF_8));
     }
 
-    private final static String AUTHORIZATION_HEADER_KEY = "Authorization";
-    public static final String BASIC = "Basic ";
     @Override
+    @Nonnull
     public CompletableFuture<Void> authenticateRequest(@Nonnull final RequestInformation request, @Nullable final Map<String, Object> additionalAuthenticationContext) {
         request.headers.add(AUTHORIZATION_HEADER_KEY, BASIC + encoded);
         return CompletableFuture.completedFuture(null);
