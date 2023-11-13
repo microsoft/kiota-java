82b7d2bb54f04a4fc23f3d5e230672139351f044
Vincent Biret <vibiret@microsoft.com>
2023-11-13 16:13:46 -0500

    - fixes enum path parameters serialization

---
+++ components/abstractions/src/test/java/com/microsoft/kiota/serialization/mocks/TestEnum.java
@@ -0,0 +1,24 @@
+package com.microsoft.kiota.serialization.mocks;
+
+import java.util.Objects;
+import com.microsoft.kiota.serialization.ValuedEnum;
+
+public enum TestEnum implements ValuedEnum {
+	First("1"),
+	Second("2");
+	public final String value;
+    TestEnum(final String value) {
+        this.value = value;
+    }
+    @jakarta.annotation.Nonnull
+    public String getValue() { return this.value; }
+    @jakarta.annotation.Nullable
+    public static TestEnum forValue(@jakarta.annotation.Nonnull final String searchValue) {
+        Objects.requireNonNull(searchValue);
+        switch(searchValue) {
+            case "1": return First;
+            case "2": return Second;
+            default: return null;
+        }
+    }
+}
