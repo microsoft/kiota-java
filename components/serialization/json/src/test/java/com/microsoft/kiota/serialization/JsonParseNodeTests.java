ec587a100422233462b9c80a321ff8ece63a20c7
Vincent Biret <vibiret@microsoft.com>
2023-12-14 14:09:45 -0500

    - code linting
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- components/serialization/json/src/test/java/com/microsoft/kiota/serialization/JsonParseNodeTests.java
+++ components/serialization/json/src/test/java/com/microsoft/kiota/serialization/JsonParseNodeTests.java
@@ -9,12 +9,10 @@ import java.io.UnsupportedEncodingException;
 
 class JsonParseNodeTests {
     private static final JsonParseNodeFactory _parseNodeFactory = new JsonParseNodeFactory();
-    private static final JsonSerializationWriterFactory _serializationWriterFactory =
-            new JsonSerializationWriterFactory();
     private static final String contentType = "application/json";
 
     @Test
-    void ItDDoesNotFailForGetChildElementOnMissingKey() throws UnsupportedEncodingException {
+    void itDDoesNotFailForGetChildElementOnMissingKey() throws UnsupportedEncodingException {
         final var initialString = "{displayName\": \"Microsoft Teams Meeting\"}";
         final var rawResponse = new ByteArrayInputStream(initialString.getBytes("UTF-8"));
         final var parseNode = _parseNodeFactory.getParseNode(contentType, rawResponse);
