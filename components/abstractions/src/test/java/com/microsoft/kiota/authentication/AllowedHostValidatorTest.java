043dc0e7feaa30177f4c94d90cbe288c163b3b55
Philip Gichuhi <pgichuhi@microsoft.com>
2024-02-01 13:15:56 +0300

    Resolve spotbugs errors

--- components/abstractions/src/test/java/com/microsoft/kiota/authentication/AllowedHostValidatorTest.java
+++ components/abstractions/src/test/java/com/microsoft/kiota/authentication/AllowedHostValidatorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class AllowedHostValidatorTest {
 
     @Test
-    public void ThrowsExceptionForHttpOrHttpsHosts() {
+    public void throwsExceptionForHttpOrHttpsHosts() {
         assertThrows(
                 IllegalArgumentException.class,
                 () ->
