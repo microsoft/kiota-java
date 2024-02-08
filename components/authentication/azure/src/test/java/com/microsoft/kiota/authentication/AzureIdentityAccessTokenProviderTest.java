5551fc552ec8703329bf155bf7794acddf9d4dd8
Philip Gichuhi <pgichuhi@microsoft.com>
2024-02-08 14:34:00 +0300

    Allow https on localhost URLs

---
+++ components/authentication/azure/src/test/java/com/microsoft/kiota/authentication/AzureIdentityAccessTokenProviderTest.java
@@ -0,0 +1,44 @@
+package com.microsoft.kiota.authentication;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import com.azure.core.credential.AccessToken;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.credential.TokenRequestContext;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+
+public class AzureIdentityAccessTokenProviderTest {
+
+    @ParameterizedTest
+    @ValueSource(
+            strings = {"http://localhost:80/me", "http://127.0.0.1/me", "http://[::1]:8080/me"})
+    void testLocalhostHttpUrlIsValid(String urlString) throws URISyntaxException {
+        var tokenCredential = mock(TokenCredential.class);
+        when(tokenCredential.getTokenSync(any(TokenRequestContext.class)))
+                .thenReturn(new AccessToken("token", null));
+        var accessTokenProvider = new AzureIdentityAccessTokenProvider(tokenCredential, null, "");
+        assertEquals(
+                "token",
+                accessTokenProvider.getAuthorizationToken(new URI(urlString), new HashMap<>()));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"http://graph.microsoft.com/me"})
+    void testNonLocalhostHttpUrlIsInvalid(String urlString) {
+        var tokenCredential = mock(TokenCredential.class);
+        var accessTokenProvider = new AzureIdentityAccessTokenProvider(tokenCredential, null, "");
+        assertThrows(
+                IllegalArgumentException.class,
+                () ->
+                        accessTokenProvider.getAuthorizationToken(
+                                new URI(urlString), new HashMap<>()));
+    }
+}
