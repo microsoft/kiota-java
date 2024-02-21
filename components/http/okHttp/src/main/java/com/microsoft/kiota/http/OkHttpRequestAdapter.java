13ae40465225894fd7b693fe96ceaaf2653959af
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2024-02-21 15:49:19 -0800

    Refactor to utilize okhttp class RequestBody.create() rather than implementing our own.

--- components/http/okHttp/src/main/java/com/microsoft/kiota/http/OkHttpRequestAdapter.java
+++ components/http/okHttp/src/main/java/com/microsoft/kiota/http/OkHttpRequestAdapter.java
@@ -878,42 +878,23 @@ public class OkHttpRequestAdapter implements com.microsoft.kiota.RequestAdapter 
             spanForAttributes.setAttribute(SemanticAttributes.SERVER_ADDRESS, requestURL.getHost());
             spanForAttributes.setAttribute(SemanticAttributes.URL_SCHEME, requestURL.getProtocol());
 
-            RequestBody body =
-                    requestInfo.content == null
-                            ? null
-                            : new RequestBody() {
-                                @Override
-                                public long contentLength() {
-                                    if (requestInfo.contentLength != null) {
-                                        spanForAttributes.setAttribute(
-                                                "http.request_content_length",
-                                                requestInfo.contentLength);
-                                        return requestInfo.contentLength;
-                                    } else return 0;
-                                }
 
-                                @Override
-                                public MediaType contentType() {
-                                    final Set<String> contentTypes =
-                                            requestInfo.headers.containsKey(contentTypeHeaderKey)
-                                                    ? requestInfo.headers.get(contentTypeHeaderKey)
-                                                    : new HashSet<>();
-                                    if (contentTypes.isEmpty()) {
-                                        return null;
-                                    } else {
-                                        final String contentType =
-                                                contentTypes.toArray(new String[] {})[0];
-                                        spanForAttributes.setAttribute(
-                                                "http.request_content_type", contentType);
-                                        return MediaType.parse(contentType);
-                                    }
-                                }
-
-                                @Override
-                                public void writeTo(@Nonnull BufferedSink sink) throws IOException {
-                                    sink.writeAll(Okio.source(requestInfo.content));
-                                }
-                            };
+            RequestBody body = null;
+            if (requestInfo.content != null) {
+                final Set<String> contentTypes =
+                        requestInfo.headers.containsKey(contentTypeHeaderKey)
+                                ? requestInfo.headers.get(contentTypeHeaderKey)
+                                : new HashSet<>();
+                final String contentType =
+                        contentTypes.toArray(new String[] {})[0];
+                spanForAttributes.setAttribute("http.request_content_type", contentType);
+                final MediaType mediaType = contentTypes.isEmpty() ? null : MediaType.parse(contentType);
+                byte[] bytes = Compatibility.readAllBytes(requestInfo.content);
+                spanForAttributes.setAttribute(
+                        "http.request_content_length",
+                        bytes.length);
+                body = RequestBody.create(bytes, mediaType);
+            }
 
             // https://stackoverflow.com/a/35743536
             if (body == null
@@ -954,6 +935,8 @@ public class OkHttpRequestAdapter implements com.microsoft.kiota.RequestAdapter 
                 }
             }
             return request;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         } finally {
             span.end();
         }
