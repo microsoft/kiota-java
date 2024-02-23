3894fcfc5d2555d7382b04768a3017b0b58dad1f
Ramses Sanchez-Hernandez <63934382+ramsessanchez@users.noreply.github.com>
2024-02-23 10:43:47 -0800

    Update components/http/okHttp/src/main/java/com/microsoft/kiota/http/OkHttpRequestAdapter.java
    Co-authored-by: Vincent Biret <vibiret@microsoft.com>

--- components/http/okHttp/src/main/java/com/microsoft/kiota/http/OkHttpRequestAdapter.java
+++ components/http/okHttp/src/main/java/com/microsoft/kiota/http/OkHttpRequestAdapter.java
@@ -906,9 +906,9 @@ public class OkHttpRequestAdapter implements com.microsoft.kiota.RequestAdapter 
                                     final Set<String> contentLength =
                                             requestInfo.headers.getOrDefault(
                                                     contentLengthHeaderKey, new HashSet<>());
-                                    if (contentLength.isEmpty()) {
-                                        try (final ByteArrayInputStream contentStream =
-                                                (ByteArrayInputStream) requestInfo.content) {
+                                    if (contentLength.isEmpty() && requestInfo.content instanceof ByteArrayInputStream) {
+                                        final ByteArrayInputStream contentStream = (ByteArrayInputStream) requestInfo.content;
+                                        try  {
                                             length = contentStream.available();
                                         } catch (IOException ex) {
                                             length = -1L;
