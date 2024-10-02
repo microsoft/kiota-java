28e221705283193f5018bfe336fabac47ac1a0f4
Philip Gichuhi <pgichuhi@microsoft.com>
2024-10-02 17:27:23 +0300

    Use inline comment to resolve spotless failure

--- components/http/okHttp/src/main/java/com/microsoft/kiota/http/middleware/options/UserAgentHandlerOption.java
+++ components/http/okHttp/src/main/java/com/microsoft/kiota/http/middleware/options/UserAgentHandlerOption.java
@@ -13,10 +13,7 @@ public class UserAgentHandlerOption implements RequestOption {
 
     private boolean enabled = true;
     @Nonnull private String productName = "kiota-java";
-    // x-release-please-start-version
-    @Nonnull private String productVersion = "1.5.0";
-
-    // x-release-please-end
+    @Nonnull private String productVersion = "1.5.0"; // x-release-please-version
 
     /**
      * Gets the product name to be used in the user agent header
