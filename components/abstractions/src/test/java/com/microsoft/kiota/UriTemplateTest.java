52f09d1eb8984d33efd2b6249e5772120da4acf6
espenam <espen.asphaug.myran@visbook.com>
2024-10-03 13:43:00 +0200

    fix: switch CsvFileSource delimiter to ',' in accordance with data.csv formatting changes

--- components/abstractions/src/test/java/com/microsoft/kiota/UriTemplateTest.java
+++ components/abstractions/src/test/java/com/microsoft/kiota/UriTemplateTest.java
@@ -29,7 +29,7 @@ public class UriTemplateTest {
     // Tests inspired from:
     // https://github.com/micronaut-projects/micronaut-core/blob/02992a905cf9a2279b7fe8e49927ff080cb937d5/http/src/test/groovy/io/micronaut/http/uri/UriTemplateSpec.groovy
     @ParameterizedTest
-    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1, delimiter = ';')
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1, delimiter = ',')
     void shouldProduceExpectedOutput(String template, String expected) {
         String result = StdUriTemplate.expand(template, params);
 
