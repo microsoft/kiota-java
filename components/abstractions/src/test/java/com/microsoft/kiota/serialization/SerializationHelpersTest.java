1a6738b10f08b859cbbff1d61f1885eb29deff1c
Philip Gichuhi <pgichuhi@microsoft.com>
2024-10-04 16:39:44 +0300

    Add unit tests

--- components/abstractions/src/test/java/com/microsoft/kiota/serialization/SerializationHelpersTest.java
+++ components/abstractions/src/test/java/com/microsoft/kiota/serialization/SerializationHelpersTest.java
@@ -1,6 +1,7 @@
 package com.microsoft.kiota.serialization;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -9,13 +10,16 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.microsoft.kiota.serialization.mocks.TestBackedModelEntity;
 import com.microsoft.kiota.serialization.mocks.TestEntity;
+import com.microsoft.kiota.store.BackingStoreSerializationWriterProxyFactory;
 
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class SerializationHelpersTest {
     private static final String _jsonContentType = "application/json";
@@ -90,5 +94,58 @@ class SerializationHelpersTest {
                         });
         assertEquals("[{'id':'123'}]", result);
         verify(mockSerializationWriter, times(1)).writeCollectionOfObjectValues(eq(""), any());
+    }
+
+    @Test
+    void serializesAllValuesInBackedModel() throws IOException {
+        final BackingStoreSerializationWriterProxyFactory
+                backingStoreSerializationWriterProxyFactory =
+                        new BackingStoreSerializationWriterProxyFactory(
+                                new JsonSerializationWriterFactory());
+        SerializationWriterFactoryRegistry.defaultInstance.contentTypeAssociatedFactories.put(
+                _jsonContentType, backingStoreSerializationWriterProxyFactory);
+
+        final TestBackedModelEntity entity = new TestBackedModelEntity();
+        entity.setId("123");
+        entity.setOfficeLocation("Nairobi");
+
+        entity.getBackingStore().setIsInitializationCompleted(true);
+
+        final String result = KiotaJsonSerialization.serializeAsString(entity, false);
+
+        assertFalse(entity.getBackingStore().getReturnOnlyChangedValues());
+        assertEquals("{\"id\":\"123\",\"officeLocation\":\"Nairobi\"}", result);
+
+        final String collectionResult =
+                KiotaJsonSerialization.serializeAsString(
+                        new ArrayList<>(Arrays.asList(entity)), false);
+
+        assertEquals("[{\"id\":\"123\",\"officeLocation\":\"Nairobi\"}]", collectionResult);
+    }
+
+    @Test
+    void serializesChangedValuesInBackedModel() throws IOException {
+        final BackingStoreSerializationWriterProxyFactory
+                backingStoreSerializationWriterProxyFactory =
+                        new BackingStoreSerializationWriterProxyFactory(
+                                new JsonSerializationWriterFactory());
+        SerializationWriterFactoryRegistry.defaultInstance.contentTypeAssociatedFactories.put(
+                _jsonContentType, backingStoreSerializationWriterProxyFactory);
+
+        final TestBackedModelEntity entity = new TestBackedModelEntity();
+        entity.setId("123");
+        entity.setOfficeLocation("Nairobi");
+
+        entity.getBackingStore().setIsInitializationCompleted(true);
+
+        final String result = KiotaJsonSerialization.serializeAsString(entity, true);
+
+        assertFalse(entity.getBackingStore().getReturnOnlyChangedValues());
+        assertEquals("{}", result);
+
+        final String collectionResult =
+                KiotaJsonSerialization.serializeAsString(
+                        new ArrayList<>(Arrays.asList(entity)), true);
+        assertEquals("[{}]", collectionResult);
     }
 }
