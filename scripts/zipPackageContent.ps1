8feb4d72c422f9631dcfe79d5e64f19344ba7a87
Vincent Biret <vibiret@microsoft.com>
2025-06-19 17:38:58 -0400

    ci: fix adds missing signatures
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- scripts/zipPackageContent.ps1
+++ scripts/zipPackageContent.ps1
@@ -1,15 +1,18 @@
 # Checks that expected files are present & have contents after the publish process to the local cache
 param(
   [Parameter(Mandatory=$true)][string] $ArtifactId,
+  [Parameter(Mandatory=$true)][string] $PackageSegment,
   [Parameter(Mandatory=$true)][string] $Version,
   [Parameter(Mandatory=$true)][string] $OutputDirectory,
   [Parameter()][string] $GroupId = "com.microsoft.kiota",
-  [Parameter()][string] $MavenLocalCachePath = "~" + [System.IO.Path]::DirectorySeparatorChar + ".m2" + [System.IO.Path]::DirectorySeparatorChar + "repository"
+  [Parameter()][string] $ComponentsSegment = "..\components",
+  [Parameter()][string] $buildOutDir = "build\publishing-repository"
 )
 
+#components\bundle\build\publishing-repository\com\microsoft\kiota\microsoft-kiota-bundle\1.8.5\
+
 $groupIdPath = $GroupId -replace "\.", [System.IO.Path]::DirectorySeparatorChar
-$packagePath = Join-Path -Path $groupIdPath -ChildPath $ArtifactId
-$packageFullPath = Join-Path -Path $MavenLocalCachePath -ChildPath $packagePath -AdditionalChildPath $Version
+$packageFullPath = Join-Path -Path $PSScriptRoot -ChildPath $ComponentsSegment -AdditionalChildPath $PackageSegment, $buildOutDir, $groupIdPath, $ArtifactId, $Version
 
 Write-Output "---------------------------------------------------"
 Write-Output "Zipping package contents at $packageFullPath"
@@ -23,7 +26,6 @@ $outputFilePath = Join-Path -Path $OutputDirectory -ChildPath "$ArtifactId-$Vers
 # removing any existing file
 Remove-Item -Path $outputFilePath -ErrorAction SilentlyContinue
 # removing any xml files that are not expected in ESRP release
-Remove-Item -Path "$packageFullPath\*.xml" -ErrorAction SilentlyContinue -Verbose
 Compress-Archive -Path "$packageFullPath\*" -DestinationPath $outputFilePath
 
 exit 0
